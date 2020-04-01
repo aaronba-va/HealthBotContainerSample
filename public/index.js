f45561401cb6942818d9f193a62ddb2b80b8b337
aaronba-va <59976536+aaronba-va@users.noreply.github.com>
2020-04-01 06:15:21 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -109,7 +109,7 @@ function initBotConversation() {
                             // Use the following activity to proactively invoke a bot scenario
                             
                             triggeredScenario: {
-                                trigger: "va_menu_hybrid",
+                                trigger: "va_menu_decision_tree",
                                
                             }
                             
